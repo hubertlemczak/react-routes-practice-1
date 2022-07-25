1875e12c4ea12deceb4d794f8140041094e48b36
Hubert Lemczak <hubertlemczak@gmail.com>
2022-07-25 10:20:05 +0100

    c

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import AppsPage from './pages/Apps'
 import "./styles.css";
 
 const productsData = [
@@ -80,6 +81,9 @@ export default function App() {
             <li>
               <Link to="/products">Products</Link>
             </li>
+            <li>
+              <Link to="/apps">Apps</Link>
+            </li>
           </ul>
         </nav>
       </header>
@@ -89,6 +93,7 @@ export default function App() {
           element={<ProductsPage products={products} />}
         />
         <Route path="/" element={<Home />} />
+        <Route path="/apps" element={<AppsPage apps={apps}/>} />
       </Routes>
     </div>
   );
